bcb5ce1a8ed7e1c670620dd61a3e0bebb00b441b
ibrahimzaher <hemazaher15@gmail.com>
2026-07-14 08:50:27 +0300

    feat: integrate Tailwind CSS, Redux, and React Query
    - Added Tailwind CSS and its related plugins for styling.
    - Integrated Redux Toolkit for state management.
    - Set up React Query for data fetching and caching.
    - Created axios instance for API requests with base URL from environment variables.
    - Updated App component to include Redux Provider and QueryClientProvider.
    - Refactored CSS to use Tailwind and removed old CSS variables.
    - Added utility functions for class name merging.
    - Created a configuration file for React Query.
    - Added a components configuration file for shadcn.
    - Updated TypeScript configuration for path aliases.
    - Created a .env file for environment variables.

---
+++ src/config/queryClient.ts
@@ -0,0 +1,12 @@
+import { QueryClient } from "@tanstack/react-query";
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      retry: 1,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { queryClient } from '@/config/queryClient'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import { store } from '@/store/store'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 )
