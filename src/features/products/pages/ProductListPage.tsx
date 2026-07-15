58e4088faf272f383665fdab8fd6e6f420d40e7d
ibrahimzaher <hemazaher15@gmail.com>
2026-07-15 14:43:52 +0300

    feat: implement product pagination component and integrate into product list page

--- src/features/products/pages/ProductListPage.tsx
+++ src/features/products/pages/ProductListPage.tsx
@@ -1,9 +1,81 @@
+import { useAppSelector } from "@/core/store/store";
+import { useState } from "react";
+import { ProductCard } from "@/shared/components/ui/ProductCard";
+import { useGetProducts } from "../hooks/useProducts";
+import { ProductPagination } from "@/features/products/components/ui/PaginatorProduct";
+
 export default function ProductListPage() {
+  const user = useAppSelector((state) => state.auth.user);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 12;
+
+  const { data, isLoading, error } = useGetProducts(
+    user?.id || "",
+    currentPage,
+    pageSize,
+  );
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center py-20">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    );
+  if (error)
+    return (
+      <div className="flex items-center justify-center py-20 text-red-500">
+        Error loading products. Please try again later.
+      </div>
+    );
+
+  const products = data?.data?.items || [];
+  const totalCount = data?.data?.totalCount || 0;
+  const totalPages = data?.data?.totalPages || 1;
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold bg-primary text-primary-foreground p-4 rounded-lg text-center">
-        Product List
-      </h1>
-    </div>
+    <>
+      <div className="flex gap-8 relative">
+        <div className="w-1/4 hidden md:block bg-gray-50"></div>
+
+        <div className="flex-1 flex flex-col justify-between">
+          <div>
+            <div className="flex justify-between flex-row items-center mb-4.5">
+              <h1 className="text-2xl md:text-3xl font-bold text-black">
+                Products
+              </h1>
+              <p className="text-xs md:text-sm text-gray-500">
+                Showing {(currentPage - 1) * pageSize + 1}-
+                {Math.min(currentPage * pageSize, totalCount)} of {totalCount}{" "}
+                Products
+              </p>
+            </div>
+
+            {products.length > 0 ? (
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8">
+                {products.map((product) => {
+                  return <ProductCard key={product.id} product={product} />;
+                })}
+              </div>
+            ) : (
+              <div className="text-center py-20 text-gray-500">
+                No products found.
+              </div>
+            )}
+          </div>
+
+          {totalPages > 1 && (
+            <ProductPagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={(newPage) => {
+                setCurrentPage(newPage);
+                window.scrollTo({ top: 0, behavior: "smooth" });
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </>
   );
 }
--- src/shared/components/ui/ProductCard.tsx
+++ src/shared/components/ui/ProductCard.tsx
@@ -1,11 +1,46 @@
 import placeholderImage from "@/assets/images/placeholder.png";
 import type { Product } from "@/features/products/types/product.types";
+import { Star, StarHalf } from "lucide-react";
 const renderStars = (rating = 4.5) => {
+  const totalStars = 5;
+  const fullStarsCount = Math.floor(rating);
+  const hasHalfStar = rating % 1 >= 0.5;
   return (
-    <div className="flex items-center gap-1 text-yellow-400 text-sm">
-      {"★".repeat(Math.floor(rating))}
-      {rating % 1 !== 0 && "½"}
-      <span className="text-gray-400 text-xs ml-1">{rating}/5</span>
+    <div className="flex items-center gap-1">
+      <div className="flex items-center gap-0.5 text-[#FFC107]">
+        {" "}
+        {Array.from({ length: totalStars }).map((_, index) => {
+          const starNumber = index + 1;
+
+          if (starNumber <= fullStarsCount) {
+            return (
+              <Star
+                key={index}
+                className="h-4 w-4 fill-current stroke-current"
+              />
+            );
+          } else if (starNumber === fullStarsCount + 1 && hasHalfStar) {
+            return (
+              <StarHalf
+                key={index}
+                className="h-4 w-4 fill-current stroke-current"
+              />
+            );
+          } else {
+            return (
+              <Star
+                key={index}
+                className="h-4 w-4 text-gray-200 fill-gray-100 stroke-gray-300"
+              />
+            );
+          }
+        })}
+      </div>
+
+      <span className="text-black text-xs font-medium ml-1.5 mt-0.5">
+        {rating}
+        <span className="text-gray-400 font-normal">/5</span>
+      </span>
     </div>
   );
 };
@@ -33,7 +68,7 @@ export const ProductCard = ({ product }: { product: Product }) => {
           {product.name}
         </h2>
 
-        <div className="mb-1">{renderStars()}</div>
+        <div className="mb-1">{renderStars(4.5)}</div>
 
         <div className="flex items-center gap-2">
           <p className="text-xl md:text-2xl font-bold text-black">
