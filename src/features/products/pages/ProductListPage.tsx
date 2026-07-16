a32b112e4541cfbbb4640056600b3027d7964091
ibrahimzaher <hemazaher15@gmail.com>
2026-07-16 09:08:45 +0300

    feat: add product filtering and mobile sheet component to ProductListPage

--- src/features/products/pages/ProductListPage.tsx
+++ src/features/products/pages/ProductListPage.tsx
@@ -1,12 +1,22 @@
 import { useAppSelector } from "@/core/store/store";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { ProductCard } from "@/shared/components/ui/ProductCard";
 import { useGetProducts } from "../hooks/useProducts";
 import { ProductPagination } from "@/features/products/components/ui/PaginatorProduct";
+import { ProductFilters } from "../components/ui/FilterProducts";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/shared/components/ui/sheet";
+import { SlidersHorizontal } from "lucide-react";
 
 export default function ProductListPage() {
   const user = useAppSelector((state) => state.auth.user);
-
+  const [priceFilter, setPriceFilter] = useState<[number, number] | null>(null);
+  const [isMobileSheetOpen, setIsMobileSheetOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 12;
 
@@ -15,6 +25,14 @@ export default function ProductListPage() {
     currentPage,
     pageSize,
   );
+  const products = useMemo(() => data?.data?.items || [], [data]);
+  const filteredProducts = useMemo(() => {
+    if (!priceFilter) return products;
+    return products.filter(
+      (product) =>
+        product.amount >= priceFilter[0] && product.amount <= priceFilter[1],
+    );
+  }, [products, priceFilter]);
 
   if (isLoading)
     return (
@@ -29,14 +47,19 @@ export default function ProductListPage() {
       </div>
     );
 
-  const products = data?.data?.items || [];
   const totalCount = data?.data?.totalCount || 0;
   const totalPages = data?.data?.totalPages || 1;
-
+  const handleApplyFilters = (priceRange: [number, number]) => {
+    setPriceFilter(priceRange);
+    setIsMobileSheetOpen(false);
+    scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <>
       <div className="flex gap-8 relative">
-        <div className="w-1/4 hidden md:block bg-gray-50"></div>
+        <div className="w-1/4 hidden lg:block border border-gray-100 rounded-[20px] p-6 h-fit bg-white sticky top-25">
+          <ProductFilters onApplyFilters={handleApplyFilters} />
+        </div>
 
         <div className="flex-1 flex flex-col justify-between">
           <div>
@@ -44,16 +67,42 @@ export default function ProductListPage() {
               <h1 className="text-2xl md:text-3xl font-bold text-black">
                 Products
               </h1>
-              <p className="text-xs md:text-sm text-gray-500">
-                Showing {(currentPage - 1) * pageSize + 1}-
-                {Math.min(currentPage * pageSize, totalCount)} of {totalCount}{" "}
-                Products
-              </p>
+              <div className="flex items-center gap-4">
+                <p className="text-xs md:text-sm text-gray-500">
+                  Showing {(currentPage - 1) * pageSize + 1}-
+                  {Math.min(currentPage * pageSize, totalCount)} of {totalCount}{" "}
+                  Products
+                </p>
+                <div className="lg:hidden">
+                  <Sheet
+                    open={isMobileSheetOpen}
+                    onOpenChange={setIsMobileSheetOpen}
+                  >
+                    <SheetTrigger asChild>
+                      <button className="bg-[#F0EEED] hover:bg-neutral-200 p-3 rounded-full text-black transition">
+                        <SlidersHorizontal className="h-5 w-5" />
+                      </button>
+                    </SheetTrigger>
+                    <SheetContent
+                      side="bottom"
+                      className="rounded-t-[20px] px-6 pb-8 pt-6 max-h-[85vh] overflow-y-auto"
+                    >
+                      <SheetHeader className="hidden">
+                        <SheetTitle>Filters</SheetTitle>
+                      </SheetHeader>
+                      <ProductFilters
+                        onApplyFilters={handleApplyFilters}
+                        onCloseMobile={() => setIsMobileSheetOpen(false)}
+                      />
+                    </SheetContent>
+                  </Sheet>
+                </div>
+              </div>
             </div>
 
-            {products.length > 0 ? (
+            {filteredProducts.length > 0 ? (
               <div className="grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-8">
-                {products.map((product) => {
+                {filteredProducts.map((product) => {
                   return <ProductCard key={product.id} product={product} />;
                 })}
               </div>
