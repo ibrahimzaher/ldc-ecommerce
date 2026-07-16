829965e624265721f375f22d7e3d0fd60f20dc58
ibrahimzaher <hemazaher15@gmail.com>
2026-07-16 12:35:11 +0300

    feat: add onClick handler to ProductCard in ProductMayLike component for navigation

--- src/features/ecommerce/pages/ProductDetails/components/ProductMayLike.tsx
+++ src/features/ecommerce/pages/ProductDetails/components/ProductMayLike.tsx
@@ -2,9 +2,11 @@ import { useAppSelector } from "@/core/store/store";
 import { ProductCard } from "@/features/ecommerce/components/ProductCard";
 import { useGetProducts } from "@/features/ecommerce/hooks/useProducts";
 import { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ProductMayLike() {
   const user = useAppSelector((state) => state.auth.user);
+  const navigate = useNavigate();
   const {
     data: productsData,
     isLoading,
@@ -35,7 +37,11 @@ export default function ProductMayLike() {
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard
+            onClick={() => navigate(`/products/${product.id}`)}
+            key={product.id}
+            product={product}
+          />
         ))}
       </div>
     </div>
